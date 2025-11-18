d19b86f56ca9d2b8823bf7ae90d38e9409bd1b32
yohaboy <yohannesgizaw777@gmail.com>
2025-11-18 08:23:48 +0300

    Navbar-update

--- components/base/NavBar.tsx
+++ components/base/NavBar.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 export default function Navigation() {
   return (
     <nav className="sticky top-0 z-50 w-full border-b border-border/50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
+      <div className=" mx-16 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
         <div className="flex items-center gap-2">
           <Link href="/" className="font-extrabold text-xl hover:cursor-pointer">VibeCN</Link>
         </div>
