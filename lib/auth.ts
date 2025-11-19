fddca6a8d0d8cf2e39a581ac8d5e295634427d8f
yohaboy <yohannesgizaw777@gmail.com>
2025-11-19 13:41:55 +0300

    auth-init

---
+++ lib/auth.ts
@@ -0,0 +1,24 @@
+import {betterAuth} from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { PrismaClient } from "@/generated/prisma/client"
+import { nextCookies } from "better-auth/next-js"
+
+const prisma = new PrismaClient()
+
+export const auth = betterAuth({
+    database:prismaAdapter(prisma,{provider:"postgresql"}),
+    emailAndPassword:{
+        enabled:true,
+    },
+    user: {
+        additionalFields: {
+          tokens: {
+            type: 'number',     
+            required: false,      
+            defaultValue: '10', 
+            input: true
+          }
+        }
+    },
+    plugins:[nextCookies()]
+})
