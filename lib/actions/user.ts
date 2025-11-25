39cd2e3090975fb267e7d92387237190f9a4a54f
yohaboy <yohannesgizaw777@gmail.com>
2025-11-26 00:58:38 +0300

    updated-fetching

--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -2,9 +2,15 @@
 
 import { Polar } from "@polar-sh/sdk";
 import  prisma from "../prisma";
+import { authClient } from "../auth-client";
 
 const polarClient = new Polar({
-  accessToken: process.env.POLAR_ACCESS_TOKEN!,
+  accessToken: process.env.POLAR_SANDBOX_ACCESS_TOKEN!,
+});
+
+const orgClient = new Polar({
+  accessToken: process.env.POLAR_SANDBOX_ACCESS_TOKEN!,
+  server: "sandbox",
 });
 
 export async function syncPolarCustomerId(userId: string) {
@@ -23,3 +29,24 @@ export async function syncPolarCustomerId(userId: string) {
     console.error("Error syncing Polar customer:", error);
   }
 }
+
+
+export async function getCustomerTransactions(polarCustomerId: string) {
+  const res = await orgClient.orders.list({
+    customerId: polarCustomerId,
+    page: 1,
+    limit: 50,
+  });
+
+  const importantOrders = res.result.items.map(order => ({
+    createdAt: order.createdAt.toISOString(),
+    totalAmount: order.totalAmount,
+    status: order.status,
+    invoiceNumber: order.invoiceNumber,
+  }));
+
+  return importantOrders;
+}
+
+
+
